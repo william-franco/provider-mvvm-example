118f632d5185b4aa28d9991fd4806bfdd5396b51
William Franco <williamff@outlook.com>
2023-08-24 21:12:17 -0300

    update project sdk, dio and go_router

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -3,7 +3,7 @@
 // This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
 //
 
-// @dart = 3.0
+// @dart = 3.1
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:shared_preferences_android/shared_preferences_android.dart';
